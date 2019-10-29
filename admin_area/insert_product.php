1b58d8dc40c7d9d8da7f710bd495f8667cedef18
Marcin <marcin96semkowski@gmail.com>
2019-10-29 19:01:07 +0100

    Adding  insert_product

---
+++ admin_area/insert_product.php
@@ -0,0 +1,77 @@
+<!DOCTYPE>
+<html>
+<head>
+    
+    
+    </head>
+
+<body bgcolor="skyblue">
+    <form action="insert_product.php" method="post" enctype="multipart/form-data">
+    
+    <table align="center" width="750" border="2" bgcolor="orange">
+        
+        <tr>
+        <td colspan="7"><h2>Insert New Post Here</h2> </td>
+        </tr>
+        <tr>
+        <td align="center"><b>Product Title:</b></td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+        
+              <tr>
+        <td align="center"><b>Product Category:</b></td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+        
+              <tr>
+        <td align="center"><b>Product Brand:</b></td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+        
+              <tr>
+        <td align="center"><b>Product Image:</b> </td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+        
+              <tr>
+        <td align="center"><b>Product Price:</b></td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+        
+              <tr>
+        <td align="center"><b>Product Description:</b></td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+        
+              <tr>
+        <td align="center"><b>Product Keywords:</b></td>
+            <td><input type="text" name="product_titile" /></td>
+        </tr>
+              <tr align="center">
+            <td colspan="7"><input type="submit" name="insert_post" value="Insert Now " /> </td>
+        </tr>
+    
+        
+    </table>
+        
+    </form>
+    </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>

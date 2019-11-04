b61633e5c4b053a507c15c7976dcd4e6d244226b
Marcin <marcin96semkowski@gmail.com>
2019-11-04 17:29:40 +0100

    adding product to database #3

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -94,6 +94,8 @@ include("includes/db.php");
         </tr>
     
         
+        
+        
     </table>
         
     </form>
@@ -115,12 +117,20 @@ if(isset($_POST['insert_post'])){
     
     $product_image_tmp = $_FILES['product_image']['tmp_name'];
         
+    move_uploaded_file($product_image_tmp,"product_images/$product_image");
 
-     echo $insert_product = "INSERT INTO products(product_cat,product_brand,product_title,product_price,product_desc,products_image,product_keywords) VALUES('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image' ,'$product_keywords' )";    
+     $insert_product = "insert into products (product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+		 
+		 $insert_pro = mysqli_query($con, $insert_product);
+		 
+		 if($insert_pro){
+		 
+		 echo "<script>alert('Product Has been inserted!')</script>";
+		 echo "<script>window.open('insert_product.php','_self')</script>";
+		 
+		 }
     
-    
-    
-    
+ 
 }
 
 ?>

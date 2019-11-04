f610b36e99f06f94fdc8cac4e60ac6dbd97cac58
Marcin <marcin96semkowski@gmail.com>
2019-11-04 18:00:40 +0100

    Get product function

--- functions/functions.php
+++ functions/functions.php
@@ -42,5 +42,38 @@ function getBrands(){
 
 
 
+function getPro(){
+    
+    global $con;
+ 
+    $get_prodcts = "SELECT * FROM products order by RAND() LIMIT 0,6";
+    
+$run_products = mysqli_query($con,$get_products);
+    
+    
+    while($row_products= mysqli_fetch_array($run_products)){
+        $pro_id = $row_products['product_id'];
+        $pro_cat = $row_products['product_cat'];
+        $pro_brand = $row_products['product_brand'];
+        $pro_title = $row_products['product_title'];
+        $pro_price = $row_products['product_price'];
+        $pro_desc = $row_products['product_desc'];
+        $pro_image = $row_products['product_image']; 
+    }
+    
+    
+    echo "<div id='single_product'>
+      <h3>$pro_title</h3>
+      <img src='admin_area/product_images/$pro_image' width='180' height='180'/>
+      
+    
+    
+    </div>";
+    
+
+}
+
+
+
 
 

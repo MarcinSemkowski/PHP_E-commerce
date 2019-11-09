8232cffe8d82002f9b43cfb4480a8b26c440d151
Marcin <marcin96semkowski@gmail.com>
2019-11-09 14:19:57 +0100

    Customer account #1

--- customer/my_account.php
+++ customer/my_account.php
@@ -1,8 +1,8 @@
 <!DOCTYPE>
 <?php
 session_start();
-include("functions/functions.php");
-include("admin_area/includes/db.php")
+include("../functions/functions.php");
+include("../admin_area/includes/db.php")
 ?>
 
 
@@ -20,17 +20,15 @@ include("admin_area/includes/db.php")
     
         <div class="header_wrapper">
         
-            <img id="logo" src="images/logo.gif"/>
-            <img id="banner" src="images/ad_banner.gif"/>
+            <img id="logo" src="../images/logo.gif"/>
+            <img id="banner" src="../images/ad_banner.gif"/>
             
             
             
             <div class="menubar">
               <ul id="menu">
                   <li><a href="index.php" >Home</a> </li>
-                   <li><a href="all_product.php" >All Products</a> </li>
-                   <li><a href="customer/my_account.php" >My Account</a> </li>
-                   <li><a href="#" >Sign In</a> </li>
+                   <li><a href="../all_product.php" >All Products</a> </li>
                    <li><a href="#" >Shopping Cart</a> </li>
                    <li><a href="#" >Contact Us</a> </li>
                 
@@ -51,18 +49,15 @@ include("admin_area/includes/db.php")
         <div class="content_wrapper">
         <div id="sidebar">
             
-            <div id="sidebar_title">Categories</div>
+            <div id="sidebar_title">My Account</div>
             <ul id="cats">
 			  
-             <?php getCats();  ?>
+             <li><a href="my_account.php?my_orders">My Orders</a></li>
+                 <li><a href="my_account.php?edit_account">Edit Account</a></li>
+                 <li><a href="my_account.php?change_pass">Change Password</a></li>
+                 <li><a href="my_account.php?delete_account">Delete Account</a></li>
               
 			</ul>
-            
-                 <div id="sidebar_title">Brands</div>
-            <ul id="cats">
-             <?php getBrands(); ?>
-               
-            </ul>
             
             </div>
             
@@ -73,13 +68,11 @@ include("admin_area/includes/db.php")
              <div id="content_area">
                  <?php cart(); ?>
              <div id="shopping_cart">
-            <span style="float:right font-size:15px; padding 5px; line-height: 40px;" f>
+            <span style="float:right font-size:15px; padding 5px; line-height: 40px;" >
                 
                <?php 
                    if(isset($_SESSION['customer_email'])){
-                       echo"<b>Welcome</b> ".$_SESSION['customer_email']." <b style='colr:yellow;'>Your</b>";
-                   }else{
-                        echo "Welcome Guest !";    
+                       echo"<b>Welcome</b> ".$_SESSION['customer_email'];
                    }
                 
                 
@@ -87,10 +80,6 @@ include("admin_area/includes/db.php")
                 ?> 
                  
                 
-                
-                
-                <b style="color:yellow">Shopping Cart -</b>Total Items:     <?php echo  total_items(); ?> Total Price: <?php  echo  total_price(); ?> $ <a href="cart.php" style="color:yellow"> Go to Cart ! </a>
-                 
                  <?php 
                 if(!isset($_SESSION['customer_email'])){
                     echo "<a href='checkout.php' style='color:orange'>Login</a>";
@@ -113,9 +102,6 @@ include("admin_area/includes/db.php")
             
         <div id="products_box">
             
-            <?php  getPro();?>
-             <?php  getCatPro(); ?>
-            <?php getBrandPro(); ?>
             </div>
         
         </div>

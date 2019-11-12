78a88dcd42c53720b9f8a052244b94303de26681
Marcin <marcin96semkowski@gmail.com>
2019-11-12 14:18:47 +0100

    Admin area layout

---
+++ admin_area/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+
+
+<html>
+<head>
+    <title>This is Admin  Panel</title>
+    
+    <link rel="stylesheet" href="styles/style.css" media="all"/>
+    </head>
+
+
+<body>
+        <div class="main_wrapper">
+    <div id="header"></div>
+    </div>
+    
+    <div id="right">Menage Contents</div>
+    <div id="left"></div>
+    
+    
+    
+    </body>
+
+</html>

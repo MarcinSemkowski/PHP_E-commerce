11dd95dd8e3a94b5ee048617f39989c6ccea12aa
Marcin <marcin96semkowski@gmail.com>
2019-11-12 13:49:43 +0100

    Delete Account #2

---
+++ customer/delete_account.php
@@ -0,0 +1,12 @@
+
+<br>
+<h2 style="text-align:center; " >Do you relly want to delete tour account</h2>
+<form action="my_account.php" method="post">
+<br><br>
+<input type="submit" name="yes" value="Yes I Want">
+<input type="submit" name="no" value="No I was joking ">
+
+</form>
+
+
+

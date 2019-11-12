db66f9fd47329a5898418124562df9c3ff69f099
Marcin <marcin96semkowski@gmail.com>
2019-11-12 14:37:20 +0100

    Admin area layout #2

--- admin_area/index.php
+++ admin_area/index.php
@@ -14,8 +14,26 @@
     <div id="header"></div>
     </div>
     
-    <div id="right">Menage Contents</div>
-    <div id="left"></div>
+    <div id="right">
+    
+    <h2 style="text-align:center;">Menage Content</h2>
+        <a href="index.php?insert_product">Insert Product</a>
+        <a href="index.php?view_products">View All Product</a>
+        <a href="index.php?insert_category">Insert New Category</a>
+        <a href="index.php?view_categories">View All Categories</a>
+        <a href="index.php?insert_brand">Insert Brand</a>
+        <a href="index.php?view_brands">View All Brands </a>
+        <a href="index.php?view_orders">View Orders</a>
+        <a href="index.php?insert_product">View All Customers</a>
+        <a href="index.php?view_payments">Viewv Payments</a>
+        <a href="logout.php">Admin Logout</a>
+        
+        
+    
+    </div>
+    <div id="left">
+    
+    </div>
     
     
     

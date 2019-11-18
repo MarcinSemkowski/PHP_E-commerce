a9567ac5b9bbd711447eafb265fdb9c260de1d4c
Marcin <marcin96semkowski@gmail.com>
2019-11-18 14:31:54 +0100

    OOP PHP Update #8 Customer

---
+++ classes/Customer.php
@@ -0,0 +1,101 @@
+<?php 
+
+class Customer{
+
+private $name;
+private $pass;
+private $country;
+private $email;
+private $image
+private $imageTmp;
+private $city;
+private $contact;
+private $adress
+
+
+public function setName($name){
+  $this->name = $name;
+}
+
+
+public function setPass($pass){
+  $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
+  $this->pass = $pass_hash;
+}
+
+
+public function setCountry($country){
+  $this->country = $country;
+}
+
+public function setEmail($email){
+  $this->email = $email;
+}
+
+public function setImage($image){
+  $this->image = $image;
+}
+
+public function setImageTmp($imageTmp){
+  $this->imgTmp = $imageTmp;
+}
+
+
+public function setCity($city){
+  $this->city = $city;
+}
+
+public function setContact($conctact){
+  $this->contact = $contact;
+}
+
+public function setAdress($adress){
+  $this->adress = $adress;
+}
+
+
+public function getName(){
+	return $this->name;
+}
+
+
+public function getPass(){
+	return $this->pass;
+}
+
+public function getCountry(){
+	return $this->country;
+}
+
+public function getEmail(){
+	return $this->email;
+}
+
+
+public function getImage(){
+	return $this->image;
+}
+
+public function getImageTmp(){
+	return $this->imageTmp;
+}
+
+
+public function getCity(){
+	return $this->city;
+}
+
+public function getContact(){
+	return $this->contact;
+}
+
+public function getAdress(){
+	return $this->adress;
+}
+
+}
+
+
+
+
+?>

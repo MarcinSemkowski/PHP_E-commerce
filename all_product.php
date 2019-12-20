fed461a8fde0ff477d39e8400208a1b2f0f3a005
Marcin <marcin96semkowski@gmail.com>
2019-12-20 22:44:18 +0100

    Bug Fix

--- all_product.php
+++ all_product.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE>
 <?php
 include("classes/AbstractDatabaseConnection.php");  
--- customer_register.php
+++ customer_register.php
@@ -92,85 +92,95 @@ $cart = new Cart();
                     }
                    ?>
              <div id="shopping_cart">
-            <span style="float:right">Welcome Guest ! <b style="color:yellow">Shopping Cart -</b>Total Items:     <?php echo  $cart->totalItems(); ?> Total Price: <?php  echo  $cart->totalPrice(); ?> $ <a href="cart.php" style="color:yellow"> Go to Cart ! </a></span>
-            
-                 </div> 
-               
-    
-                 
-           <form  method="post" enctype="multipart/form-data">
-                 
-                  <table align="center" width="900">
-               
-                      <tr align="center">
-                       <td colspan="8"><h2>Create an Account</h2></td>  
-                      </tr> 
+           <span style="float:right; font-size:18px; padding:5px; line-height:40px;">
+					
+					Welcome Guest! <b style="color:yellow">Shopping Cart -</b> Total Items: <?php $cart->totalItems();?> Total Price: <?php $cart->totalPrice(); ?> <a href="cart.php" style="color:yellow">Go to Cart</a>
+					
+					
+					
+					</span>
+            	
 
-                      <tr>
-                       <td align="right">Customer Name:</td>
-                          <td><input type="text" name="c_name" required /></td>
-                      </tr>
-                      
-                       <tr>
-                       <td align="right">Customer Email</td>
-                          <td><input type="text" name="c_email"  required/></td>
-                      </tr>
-                      
-                       <tr>
-                       <td align="right">Cutomer Password</td>
-                          <td><input type="password" name="c_pass" required /></td>
-                      </tr>
-                      
-                       <tr>
-                       <td align="right">Cutomer Image</td>
-                          <td><input type="file" name="c_image" required /></td>
-                      </tr>
-                      
-                       <tr>
-                       <td align="right">Customer Country</td>
-                          <td><select name="c_country" required>
-                           <option>Select a Country</option>
-                              <option>Afganistan</option>
-                              <option>India</option>
-                              <option>Japan</option>
-                              <option>Pakistan</option>
-                              <option>Israel</option>
-                              <option>United Arab Emirates</option>
-                              <option>United States</option>
-                              <option>United Kingdom</option>
-                           </select>
-                           </td>
-                           
-                      </tr>
-                      
-                       <tr>
-                       <td align="right">Customer City</td>
-                          <td><input type="text" name="c_city" /></td>
-                      </tr> 
-                      
-                       <tr>
-                       <td align="right">Customer Contact</td>
-                          <td><input type="text" name="c_contact" /></td>
-                      </tr> 
-                      
-                       <tr>
-                       <td align="right">Customer Adress</td>
-                          <td><input type="text" name="c_adress" /></td>
-                      </tr> 
-               
-                      <tr>
-                      <td align="right"><input type="submit" name="regiester" value="Create Account" /></td>
-                      </tr>
-                     
-               
-               </table>               
-                 
-                 
-                 </form>
-        
+
+            	<form action="customer_register.php" method="post" enctype="multipart/form-data">
+					
+					<table align="center" width="750">
+						
+						<tr align="center">
+							<td colspan="6"><h2>Create an Account</h2></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Name:</td>
+							<td><input type="text" name="c_name" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Email:</td>
+							<td><input type="text" name="c_email" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Password:</td>
+							<td><input type="password" name="c_pass" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Image:</td>
+							<td><input type="file" name="c_image" required/></td>
+						</tr>
+						
+						
+						
+						<tr>
+							<td align="right">Customer Country:</td>
+							<td>
+							<select name="c_country">
+								<option>Select a Country</option>
+								<option>Afghanistan</option>
+								<option>India</option>
+								<option>Japan</option>
+								<option>Pakistan</option>
+								<option>Israel</option>
+								<option>Nepal</option>
+								<option>United Arab Emirates</option>
+								<option>United States</option>
+								<option>United Kingdom</option>
+							</select>
+							
+							</td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer City:</td>
+							<td><input type="text" name="c_city" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Contact:</td>
+							<td><input type="text" name="c_contact" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Address</td>
+							<td><input type="text" name="c_address" required/></td>
+						</tr>
+						
+						
+					<tr align="center">
+						<td colspan="6"><input type="submit" name="register" value="Create Account" /></td>
+					</tr>
+					
+					
+					
+					</table>
+				
+				</form>
+                
         </div>
             
-            </div >
+           
+            </div>
             
             
         <div id="footer">
@@ -179,7 +189,7 @@ $cart = new Cart();
     
     
     
-    
+    </div>
     
     </body>
     
